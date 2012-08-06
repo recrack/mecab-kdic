1bda2f3d2a1cf6031b39c9c1daf52322831c70c4
Hodong Kim <cogniti@gmail.com>
2012-08-07 06:43:19 +0900

    NFD로 변환하여 사전파일을 만든다

--- csv-gen.rb
+++ csv-gen.rb
@@ -1,15 +1,51 @@
 #!/usr/bin/ruby
 # coding: utf-8
-
+#
 # kdic.txt 로부터 kdic.csv 를 생성하는 소스이다.
+#
+#' 안녕하십니까' 를 분해하면
+#
+# 안녕   일반명사
+# 하     동사파생접미사
+# 시     선어말어미
+# ㅂ니까 종결어미
+#
+# 이렇게 분해가 되는데, 'ㅂ니까' 같은 형태 때문에 한글을 nfd 로 풀어서 corpus,
+# kdic.csv 를 작성해야 한다.
+#
+# 실제 형태소 분석기를 사용할 때에는 "안녕하십니까" 문장을 nfd 로 풀어서 mecab에
+# 입력하여 결과값을 받아 nfc 로 묶어서 화면에 출력해줘야 한다.
+
 require 'csv'
+require './hangul-utils.rb'
 
 i = 0
 col_size = 10
 
+# NOTE
+# 형태소로 분리했을 때 받침ㄴ,ㅂ 등이 나왔다면 아래 테이블을 이용하여
+# 받침으로 변환해주어야 한다.
+# 단, 기억(ㄱ), 니은(ㄴ)... 이렇게 사용되었을 경우에는 명사이므로
+# 변환해서는 안 된다.
+TABLE = {
+    "ㄴ" => "\u11ab",
+    "ㅂ" => "\u11b8" # 받침ㅂ
+}
+
 CSV.foreach("kdic.txt") do |row|
     begin
-        puts(row[0] + ",0"*3 + "," + row[1..-1].join(",") + ",*" * (col_size - row.size))
+        nfd = ""
+        row[0].each_char do |ch|
+            case ch.ord
+            # TODO 받침으로 사용되는 모든 형태소를 알아내어
+            # 그 형태소만 변환시켜야 한다.
+            when 12593..12622 # "ㄱ".."ㅎ"
+                nfd << TABLE[ch]
+            else
+                nfd << HangulUtils.decompose_hangul(ch)
+            end
+        end
+        puts(nfd + ",0"*3 + "," + row[1..-1].join(",") + ",*" * (col_size - row.size))
     rescue
         raise "at line #{i+1} column size > #{col_size}"
     end
--- test.rb
+++ test.rb
@@ -2,10 +2,38 @@
 # coding: utf-8
 
 require 'MeCab'
+require './hangul-utils.rb'
+
+# "안녕하십니까"는 안녕 + 하 + 시 + ㅂ니까 로 분해가 되므로 그 처리를 위해
+# kdic.csv 의 표층어(가장 왼쪽줄의 단어)는 NFD 형태로 입력되어 있다.
+#
+# NOTE
+# 따라서 mecab를 실제 사용할 경우
+# 한글 글자를 NFD로 풀어서 MeCab에 입력하고
+# 반환된 결과값을 NFC로 묶어줘야 한다.
+# 아래는 그 예이다.
+
+# TODO
+# 초성자음 <---> 종성자음 코드로 상호 변환하는 코드가 필요한다.
 
 m = MeCab::Tagger.new ("-d ./final")
 
 f = File.new("./text/채만식-태평천하.txt", "r")
+
 f.each_line do |line|
-    print m.parse (line) unless line.chomp.empty?
+    nfd = ""
+    # 한글 글자를 풀어준다.(NFD)
+    line.each_char {|ch| nfd << HangulUtils.decompose_hangul(ch)}
+    unless line.chomp.empty?
+        ascii_nfd = m.parse(nfd)
+        utf8_nfd = ascii_nfd.force_encoding("UTF-8")
+        # 한글 글자를 묶어준다.(NFC) 그리고 화면에 출력한다
+        untrusted = HangulUtils.compose_hangul(utf8_nfd)
+        # TODO 받침으로 분해되는 형태소가 있을 때마다
+        # 소스를 수정해야 하고 기억(ㄱ),니은(ㄴ)..은 명사로 사용되므로
+        # 혼동을 피하기 위해서라도 전체적으로 코드를 간결화할 필요가 있다.
+        # 일단은 아래처럼 사용하도록 한다.
+        trusted = untrusted.gsub("ᆸ니까", "ㅂ니까")
+        puts trusted
+    end
 end
