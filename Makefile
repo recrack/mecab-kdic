1505a598c3419c2f6c0bc6b76d0de8ae9fc4f4e7
Hodong Kim <cogniti@gmail.com>
2012-08-13 20:06:23 +0900

    유틸리티 추가

--- Makefile
+++ Makefile
@@ -1,6 +1,8 @@
 all:
-	./pos-id-gen.rb > ./seed/pos-id.def && ./pos-id-gen.rb > ./final/pos-id.def && ./csv-gen.rb > ./seed/kdic.csv
+	./pos-id-gen.rb > ./seed/pos-id.def && \
+        ./pos-id-gen.rb > ./final/pos-id.def && ./mk-corpus-dic.rb
 	cd seed && make && cd ../final && make
 
 clean:
+	rm -f corpus && \
 	cd seed && make clean && cd ../final && make clean
--- test.rb
+++ test.rb
@@ -2,7 +2,7 @@
 # coding: utf-8
 
 require 'MeCab'
-require './hangul-utils.rb'
+#require './hangul-utils.rb'
 
 # "안녕하십니까"는 안녕 + 하 + 시 + ㅂ니까 로 분해가 되므로 그 처리를 위해
 # kdic.csv 의 표층어(가장 왼쪽줄의 단어)는 NFD 형태로 입력되어 있다.
@@ -18,22 +18,29 @@ require './hangul-utils.rb'
 
 m = MeCab::Tagger.new ("-d ./final")
 
-f = File.new("./text/채만식-태평천하.txt", "r")
+if ARGV[0].nil?
+    puts "usage: ruby test.rb filename"
+    exit 0
+end
+
+f = File.new(ARGV[0], "r")
 
 f.each_line do |line|
-    nfd = ""
+    #nfd = ""
     # 한글 글자를 풀어준다.(NFD)
-    line.each_char {|ch| nfd << HangulUtils.decompose_hangul(ch)}
-    unless line.chomp.empty?
-        ascii_nfd = m.parse(nfd)
-        utf8_nfd = ascii_nfd.force_encoding("UTF-8")
+    #line.each_char {|ch| nfd << HangulUtils.decompose_hangul(ch)}
+    line.strip!
+    unless line.empty?
+        puts m.parse(line)
+        #ascii_nfd = m.parse(nfd)
+        #utf8_nfd = ascii_nfd.force_encoding("UTF-8")
         # 한글 글자를 묶어준다.(NFC) 그리고 화면에 출력한다
-        untrusted = HangulUtils.compose_hangul(utf8_nfd)
+        #untrusted = HangulUtils.compose_hangul(utf8_nfd)
         # TODO 받침으로 분해되는 형태소가 있을 때마다
         # 소스를 수정해야 하고 기억(ㄱ),니은(ㄴ)..은 명사로 사용되므로
         # 혼동을 피하기 위해서라도 전체적으로 코드를 간결화할 필요가 있다.
         # 일단은 아래처럼 사용하도록 한다.
-        trusted = untrusted.gsub("ᆸ니까", "ㅂ니까")
-        puts trusted
+        #trusted = untrusted.gsub("ᆸ니까", "ㅂ니까")
+        #puts trusted
     end
 end
